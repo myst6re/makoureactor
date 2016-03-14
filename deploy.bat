487ed8570d913bb44a91c15bd62406fb11e164a6
myst6re <myst6re@gmail.com>
2016-03-14 22:09:38 +0100

    Adding deploy part for appveyor. Removing some deploy dlls

--- deploy.bat
+++ deploy.bat
@@ -10,10 +10,10 @@ rem Create target directory
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 
 rem Deploy DLLs
-%LIB_DIR%\windeployqt.exe --force --release --dir %OUTPUT_DIR% --no-quick-import --no-translations --no-webkit2 --no-svg --no-webkit %EXE_PATH%
+%LIB_DIR%\windeployqt.exe --force --release --dir %OUTPUT_DIR% --no-quick-import --no-translations --no-webkit2 --no-angle --no-svg --no-webkit %EXE_PATH%
 
 rem Removing unused DLLs
-rem del /q %OUTPUT_DIR%\opengl32sw.dll
+del /q %OUTPUT_DIR%\opengl32sw.dll
 
 rem Deploy Translations
 for %%l in (%LANGUAGES%) do (
