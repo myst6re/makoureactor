e70f431f7f1bbd69604a6d6d7ae54bad0ffc9cbb
myst6re <myst6re@gmail.com>
2016-03-20 14:44:29 +0100

    Adding warnings to stderr

--- core/field/FieldModelFilePC.cpp
+++ core/field/FieldModelFilePC.cpp
@@ -70,6 +70,8 @@ quint8 FieldModelFilePC::load(const QString &hrc, const QString &a, bool animate
 					QImage tex = openTexture(texName % ".tex");
 					if (!tex.isNull()) {
 						_loadedTex.insert(texID, tex);
+					} else {
+						qWarning() << "FieldModelFilePC::load error texture" << hrcFilename << texName;
 					}
 					++texID;
 				}
@@ -116,6 +118,8 @@ bool FieldModelFilePC::openMesh(QMultiMap<int, QStringList> &rsdFiles, QStringLi
 		foreach (const QString &rsd, itRsd.value()) {
 			if (openPart(rsd.toLower() % ".rsd", boneID, textureFiles)) {
 				onePartOpened = true;
+			} else {
+				qWarning() << "FieldModelFilePC::openMesh cannot open part" << rsd;
 			}
 		}
 	}
@@ -135,6 +139,7 @@ bool FieldModelFilePC::openPart(const QString &rsdFileName, int boneID, QStringL
 			_skeleton[boneID].addPart(part);
 			return true;
 		} else {
+			qWarning() << "FieldModelFilePC::openPart part error" << rsdFileName << rsd.pFile();
 			delete part;
 		}
 	}
