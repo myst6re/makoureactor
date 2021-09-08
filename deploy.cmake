9a4c3aba686c19e234cae7fe7b5174f8dc1fa24c
myst6re <myst6re@gmail.com>
2021-09-09 01:42:35 +0200

    Removing qt5 prefix when possible with Qt 5.15

--- deploy.cmake
+++ deploy.cmake
@@ -6,12 +6,8 @@ elseif(WIN32)
     set(DEPLOYQT_ARGS --no-quick-import --no-webkit2 --no-angle --no-svg --no-webkit)
 endif()
 
-message("-- _qt_bin_dir: ${_qt_bin_dir}")
-message("-- TARGET_FILE_DIR: ${TARGET_FILE_DIR} ${CMAKE_BINARY_DIR}")
-
 if (APPLE OR WIN32)
     find_program(DEPLOYQT_EXECUTABLE "${DEPLOYQT_NAME}" HINTS "${_qt_bin_dir}")
-    message("-- DEPLOYQT_EXECUTABLE: ${DEPLOYQT_EXECUTABLE}")
     execute_process(
         COMMAND "${DEPLOYQT_EXECUTABLE}"
         ${DEPLOYQT_ARGS} "${TARGET_FILE_DIR}"
