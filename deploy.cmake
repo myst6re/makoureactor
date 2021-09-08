10203d6f441cd7a12bfa28cc32816e9fc32c1ed5
myst6re <myst6re@gmail.com>
2021-09-09 01:24:09 +0200

    Install for windows + QtZlib

--- deploy.cmake
+++ deploy.cmake
@@ -1,19 +1,19 @@
 if(APPLE)
     set(DEPLOYQT_NAME macdeployqt)
+    set(DEPLOYQT_ARGS)
 elseif(WIN32)
     set(DEPLOYQT_NAME windeployqt)
+    set(DEPLOYQT_ARGS --no-quick-import --no-webkit2 --no-angle --no-svg --no-webkit)
 endif()
+
+message("-- _qt_bin_dir: ${_qt_bin_dir}")
+message("-- TARGET_FILE_DIR: ${TARGET_FILE_DIR} ${CMAKE_BINARY_DIR}")
 
 if (APPLE OR WIN32)
     find_program(DEPLOYQT_EXECUTABLE "${DEPLOYQT_NAME}" HINTS "${_qt_bin_dir}")
+    message("-- DEPLOYQT_EXECUTABLE: ${DEPLOYQT_EXECUTABLE}")
     execute_process(
         COMMAND "${DEPLOYQT_EXECUTABLE}"
-        "${TARGET_FILE_DIR}"
-    )
-endif()
-
-if(WIN32)
-    set (FF7TK_DEPENDS
-        ${ff7tk_DIR}/../../../bin/ff7tk.dll
+        ${DEPLOYQT_ARGS} "${TARGET_FILE_DIR}"
     )
 endif()
