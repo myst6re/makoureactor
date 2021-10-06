282620a2603e34c6930ca8c9f9a6f40dff60153d
myst6re <myst6re@gmail.com>
2021-10-06 22:54:22 +0200

    Try to fix multi targets packed in one installer

--- deploy/deploy.cmake
+++ deploy/deploy.cmake
@@ -4,21 +4,10 @@ if(APPLE)
 elseif(WIN32)
     set(DEPLOYQT_NAME win)
     set(DEPLOYQT_ARGS --no-quick-import --no-webkit2 --no-angle --no-svg --no-webkit)
-else()
-    set(DEPLOYQT_NAME linux)
-    set(DEPLOYQT_ARGS -appimage -extra-plugins=iconengines,platformthemes/libqgtk3.so)
 endif()
 
-find_program(DEPLOYQT_EXECUTABLE "${DEPLOYQT_NAME}deployqt" HINTS "${_qt_bin_dir}")
-if(UNIX)
-    #set(ENV{QTDIR} "")
-    #set(ENV{QT_PLUGIN_PATH} "")
-    #set(ENV{LD_LIBRARY_PATH} "${ff7tk_DIR}/../..")
-    #execute_process(
-    #    COMMAND "${DEPLOYQT_EXECUTABLE}"
-    #    "${TARGET_FILE_DIR}" ${DEPLOYQT_ARGS}
-    #)
-else()
+if(APPLE OR WIN32)
+    find_program(DEPLOYQT_EXECUTABLE "${DEPLOYQT_NAME}deployqt" HINTS "${_qt_bin_dir}")
     execute_process(
         COMMAND "${DEPLOYQT_EXECUTABLE}"
         ${DEPLOYQT_ARGS} "${TARGET_FILE_DIR}"
