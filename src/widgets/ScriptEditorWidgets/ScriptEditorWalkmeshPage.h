ccb6b2aeca8bbeb5a221d766c8de54b6c3b56b0c
myst6re <myst6re@gmail.com>
2021-11-06 20:26:38 +0100

    Fix licence

--- src/widgets/ScriptEditorWidgets/ScriptEditorWalkmeshPage.h
+++ src/widgets/ScriptEditorWidgets/ScriptEditorWalkmeshPage.h
@@ -1,5 +1,21 @@
-#ifndef SCRIPTEDITORWALKMESHPAGE_H
-#define SCRIPTEDITORWALKMESHPAGE_H
+/****************************************************************************
+ ** Makou Reactor Final Fantasy VII Field Script Editor
+ ** Copyright (C) 2009-2021 Arzel Jérôme <myst6re@gmail.com>
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#pragma once
 
 #include "ScriptEditorView.h"
 #include "widgets/VertexWidget.h"
@@ -20,5 +36,3 @@ private:
 	WalkmeshWidget *_previewWalkmesh;
 	Field *_field;
 };
-
-#endif // SCRIPTEDITORWALKMESHPAGE_H
