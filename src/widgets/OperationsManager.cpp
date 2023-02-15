be5d5f22405ec8828380efb02239ad4665ea8871
Chris Rizzitello <rizzitello@kde.org>
2023-02-14 20:05:26 -0500

    remove Unneeded Include

--- src/widgets/OperationsManager.cpp
+++ src/widgets/OperationsManager.cpp
@@ -16,7 +16,6 @@
  ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 #include "OperationsManager.h"
-#include "core/field/FieldArchive.h"
 
 OperationsManager::OperationsManager(bool isPC, QWidget *parent) :
 	QDialog(parent, Qt::Dialog | Qt::WindowCloseButtonHint)
