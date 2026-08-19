7f28e661317e6a0b52ac1445366ef3e116c3fff3
myst6re <myst6re@gmail.com>
2026-08-19 21:24:45 +0200

    Credit DLPB as a contributor

--- translations/Makou_Reactor_fr.ts
+++ translations/Makou_Reactor_fr.ts
@@ -8,16 +8,24 @@
         <translation>Par Jérôme &amp;lt;myst6re&amp;gt; Arzel &lt;br/&gt;&lt;a href=&quot;https://github.com/myst6re/makoureactor/&quot;&gt;github.com/myst6re/makoureactor&lt;/a&gt;</translation>
     </message>
     <message>
+        <source>Contributors:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Sithlord48&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;dangarfield&lt;/li&gt;&lt;li&gt;vegetass4&lt;/li&gt;&lt;li&gt;nickrum&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</source>
+        <translation>Constributeurs&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Sithlord48&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;dangarfield&lt;/li&gt;&lt;li&gt;vegetass4&lt;/li&gt;&lt;li&gt;nickrum&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <source>Thanks to:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;/ul&gt;</source>
+        <translation>Merci à&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;/ul&gt;</translation>
+    </message>
+    <message>
         <source>Thanks to:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</source>
         <translation type="vanished">Merci à&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</translation>
     </message>
     <message>
         <source>Contributors:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Sithlord48&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;dangarfield&lt;/li&gt;&lt;li&gt;vegetass4&lt;/li&gt;&lt;li&gt;nickrum&lt;/li&gt;&lt;/ul&gt;</source>
-        <translation>Constributeurs&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Sithlord48&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;dangarfield&lt;/li&gt;&lt;li&gt;vegetass4&lt;/li&gt;&lt;li&gt;nickrum&lt;/li&gt;&lt;/ul&gt;</translation>
+        <translation type="vanished">Constributeurs&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Sithlord48&lt;/li&gt;&lt;li&gt;TrueOdin&lt;/li&gt;&lt;li&gt;dangarfield&lt;/li&gt;&lt;li&gt;vegetass4&lt;/li&gt;&lt;li&gt;nickrum&lt;/li&gt;&lt;/ul&gt;</translation>
     </message>
     <message>
         <source>Thanks to:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</source>
-        <translation>Merci à&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</translation>
+        <translation type="vanished">Merci à&amp;nbsp;:&lt;ul style=&quot;margin:0&quot;&gt;&lt;li&gt;Squall78&lt;/li&gt;&lt;li&gt;Synergy Blades&lt;/li&gt;&lt;li&gt;Akari&lt;/li&gt;&lt;li&gt;Asa&lt;/li&gt;&lt;li&gt;Aali&lt;/li&gt;&lt;li&gt;DLPB&lt;/li&gt;&lt;/ul&gt;</translation>
     </message>
 </context>
 <context>
